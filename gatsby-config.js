fb189f0ab99901b004f83ec21f867d9b6e23c448
Ritesh261 <sunitharitesh@gmail.com>
2024-02-10 22:04:36 -0500

    removed twitter

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,9 +5,10 @@ module.exports = {
     title: 'Ritesh Manchikanti',
     description:
       'Ritesh Manchikanti is a software engineer who specializes in building (and occasionally designing) exceptional digital experiences.',
-    siteUrl: 'https://brittanychiang.com', // No trailing slash allowed!
-    image: '/og.png', // Path to your image you placed in the 'static' folder
-    twitterUsername: '@bchiang7',
+    siteUrl: 'https://ritesh-manchikanti.netlify.app/', // No trailing slash allowed!
+    image:
+      '/Users/riteshmanchikanti/Work/resume/Ritesh_Portfolio/v4-Ritesh-Manchikanti/v4-Ritesh-Manchikanti/src/images/my_jpg.jpeg', // Path to your image you placed in the 'static' folder
+    // twitterUsername: '@bchiang7',
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
