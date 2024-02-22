c9a13a84d62eacbf9c89b27eca3d6e9eed8a3bbc
Ritesh261 <sunitharitesh@gmail.com>
2024-02-21 21:02:10 -0500

    updated tags and social media links

--- src/components/icons/loader.js
+++ src/components/icons/loader.js
@@ -13,12 +13,6 @@ const IconLoader = () => (
     <text className="cls-1" transform="translate(44.27 133.58)">
       R
     </text>
-    {/* d="M 50, 5
-                  L 11, 27
-                  L 11, 72
-                  L 50, 95
-                  L 89, 73
-                  L 89, 28 z" */}
   </svg>
 );
 
